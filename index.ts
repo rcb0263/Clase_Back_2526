433eb8f8fbd896c1e070fcf39ad427f50dcb0fe9
ciordiarafaelbello@gmail.com <rcb0263>
2025-09-29 17:00:24 +0200

    axios install

--- index.ts
+++ index.ts
@@ -1,2 +1,12 @@
-const miVariable: string = "Hola guapos"
-console.log(miVariable)
+const promesaRickYMorty = fetch("https://rickandmortyapi.com/api/character/1")
+const vueltaPromesa = promesaRickYMorty.then((response)=>{
+    const data = response.json();
+    data.then((character)=>{
+        console.log(character)
+    })
+}).catch((error)=>{
+    console.log("Error en la petición", error)
+}).finally(()=>{
+    console.log("felicidades has conseguido un personaje o no")
+})
+console.log("que tal estas")
